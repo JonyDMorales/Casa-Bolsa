48893b1b3e0c08569ec47c4df0186c18e89c9423
yovanijimenez <yovanijq@gmail.com>
2020-03-25 16:55:29 -0600

    se descomenta un metodo

--- src/main/java/com/phi/proyect/controller/LogaritmoController.java
+++ src/main/java/com/phi/proyect/controller/LogaritmoController.java
@@ -59,34 +59,46 @@ public class LogaritmoController {
 		return mav;
 	}
 
-	/*
-	 * @RequestMapping(value = "/log", method = RequestMethod.POST)
-	 * 
-	 * @ResponseBody public List<com.phi.proyect.vo.Logaritmo> lista(@RequestBody
-	 * ObjectNode obj) { String descripcion = obj.get("descripcion").asText();
-	 * String fecha = obj.get("fecha").asText(); Double tasa =
-	 * obj.get("tasa").asDouble(); List<Logaritmo> lista =
-	 * log.findByDescripcion(descripcion); List<com.phi.proyect.vo.Logaritmo>
-	 * listReturn = new ArrayList<com.phi.proyect.vo.Logaritmo>(); if
-	 * (!lista.isEmpty()) { String[] desc =
-	 * lista.get(0).getValorDelParametro().split("\\|"); if
-	 * (desc[1].equals("market_surcharge")) { List<Vector> lista2 =
-	 * vecSer.findIssue(lista.get(0).getDescripcion(), Integer.parseInt(desc[0]));
-	 * int cont = 1; for (int i = 0; i < lista2.size() - 1; i++) { Float logaritmo =
-	 * (float) 0.0; logaritmo = (float) Math .log(lista2.get(i).getMarketSurcharge()
-	 * / lista2.get(cont).getMarketSurcharge());
-	 * 
-	 * if (i == 0) { Date date1; try { date1 = new
-	 * SimpleDateFormat("yyyy-MM-dd").parse(fecha);
-	 * //System.out.println(CalculaPrecio(descripcion, date1, logaritmo + tasa)); }
-	 * catch (ParseException e) { // TODO Auto-generated catch block
-	 * e.printStackTrace(); }
-	 * 
-	 * }
-	 * 
-	 * listReturn.add(new com.phi.proyect.vo.Logaritmo(logaritmo)); cont++; } return
-	 * listReturn; } return listReturn; } return listReturn; }
-	 */
+	@RequestMapping(value = "/log", method = RequestMethod.POST)
+	@ResponseBody
+	public List<com.phi.proyect.vo.Logaritmo> lista(@RequestBody ObjectNode obj) {
+		String descripcion = obj.get("descripcion").asText();
+
+		String fecha = obj.get("fecha").asText();
+		Double tasa = obj.get("tasa").asDouble();
+		List<Logaritmo> lista = log.findByDescripcion(descripcion);
+		List<com.phi.proyect.vo.Logaritmo> listReturn = new ArrayList<com.phi.proyect.vo.Logaritmo>();
+		if (!lista.isEmpty()) {
+			String[] desc = lista.get(0).getValorDelParametro().split("\\|");
+			if (desc[1].equals("market_surcharge")) {
+				List<Vector> lista2 = vecSer.findIssue(lista.get(0).getDescripcion(), Integer.parseInt(desc[0]));
+				int cont = 1;
+				for (int i = 0; i < lista2.size() - 1; i++) {
+					Float logaritmo = (float) 0.0;
+					logaritmo = (float) Math
+							.log(lista2.get(i).getMarketSurcharge() / lista2.get(cont).getMarketSurcharge());
+
+					if (i == 0) {
+						Date date1;
+						try {
+							date1 = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+
+							// System.out.println(CalculaPrecio(descripcion, date1, logaritmo + tasa));
+						} catch (ParseException e) { // TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+
+					listReturn.add(new com.phi.proyect.vo.Logaritmo(logaritmo));
+					cont++;
+				}
+				return listReturn;
+			}
+			return listReturn;
+		}
+		return listReturn;
+	}
 
 	@PostMapping("/calcular/precio")
 	public double calcular(@RequestBody ObjectNode obj) {
@@ -112,11 +124,11 @@ public class LogaritmoController {
 				Double valor = 0.0;
 				Double multi = 0.0;
 				if (lista3.size() > 0) {
-				valor = lista3.get(0).getValorEnLibros();
+					valor = lista3.get(0).getValorEnLibros();
 				}
-				multi = valor  * Double.parseDouble(lista3.get(0).getTitulos());
+				multi = valor * Double.parseDouble(lista3.get(0).getTitulos());
 				listReturn.add(new com.phi.proyect.vo.MesadeDinero(lista.get(i).getIdValmerPriceVector(),
-						lista.get(i).getIssue(), lista2.get(0).getLimite(), valor,multi));
+						lista.get(i).getIssue(), lista2.get(0).getLimite(), valor, multi));
 			}
 		}
 		return listReturn;

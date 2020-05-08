f3cb50c11d251b1411d83b650de3e867f494680c
Alejandro Perez <perezdeleonalejandro94@gmail.com>
2020-05-08 18:49:33 -0500

    cambio y creacion para las funciones de la bd

--- src/main/java/com/phi/proyect/repository/DivisasRepository.java
+++ src/main/java/com/phi/proyect/repository/DivisasRepository.java
@@ -1,9 +1,11 @@
 package com.phi.proyect.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.phi.proyect.models.Divisas;
 
@@ -26,7 +28,17 @@ public interface DivisasRepository extends JpaRepository<Divisas,String> {
 	public List<Divisas> findFirstByTvOrderByDateDesc(String divisa);
 	
 	
+	@Query(value="SELECT DistEst(:valorEnvi);", nativeQuery =  true)
+	public float distEst(@Param("valorEnvi") Double valorEnvi);
 	
+	@Query(value="SELECT VaRCapFloor(:valorEnvi1,:valorEnvi2,:valorEnvi3,:valorEnvi4);", nativeQuery =  true)
+	public float VaRCapFloor(@Param("valorEnvi1") String valorEnvi1,@Param("valorEnvi2") Integer valorEnvi2,@Param("valorEnvi3") Date valorEnvi3,@Param("valorEnvi4") Integer valorEnvi4);
+	
+	@Query(value="SELECT VaRFwdSwapStarting(:valorEnvi1,:valorEnvi2,:valorEnvi3);", nativeQuery =  true)
+	public float VaRFwdSwapStarting(@Param("valorEnvi1") String valorEnvi1,@Param("valorEnvi2") Integer valorEnvi2,@Param("valorEnvi3") Date valorEnvi3);
+	
+	@Query(value="SELECT VaRSwapTIIE(:valorEnvi1,:valorEnvi2,:valorEnvi3);", nativeQuery =  true)
+	public float VaRSwapTIIE(@Param("valorEnvi1") String valorEnvi1,@Param("valorEnvi2") Integer valorEnvi2,@Param("valorEnvi3") Date valorEnvi3);
 	
 
 }
--- src/main/java/com/phi/proyect/service/DivisasService.java
+++ src/main/java/com/phi/proyect/service/DivisasService.java
@@ -1,5 +1,6 @@
 package com.phi.proyect.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,21 @@ public class DivisasService {
 		return this.dr.findFirstByTvOrderByDateDesc(divisa);
 	}
     
-    
+	public float distEst(Double valorEnvi) {
+		return this.dr.distEst(valorEnvi);
+	}
+	
+	public double VaRCapFloor(String valorEnvi1,int valorEnvi2,Date valorEnvi3,int valorEnvi4) {
+		return this.dr.VaRCapFloor(valorEnvi1, valorEnvi2, valorEnvi3, valorEnvi4);
+	}
+	
+	
+	public double VaRFwdSwapStarting(String valorEnvi1,int valorEnvi2,Date valorEnvi3) {
+		return this.dr.VaRFwdSwapStarting(valorEnvi1, valorEnvi2, valorEnvi3);
+	}
+	
+	public double VaRSwapTIIE(String valorEnvi1,int valorEnvi2,Date valorEnvi3) {
+		return this.dr.VaRSwapTIIE(valorEnvi1, valorEnvi2, valorEnvi3);
+	}
 
 }
